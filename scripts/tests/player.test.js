16f75f125db0efd3c1ec38c22559fcb17348d94d
AmyGois <ahagois@hotmail.com>
2025-02-04 17:29:12 +0000

    !test: create tests for Player class & PlayerBot
    Change tests for Player Class, removing requirement to define the "type"
    of player. Create tests for new PlayerBot class, as subclass of Player.

--- scripts/tests/player.test.js
+++ scripts/tests/player.test.js
@@ -1,11 +1,56 @@
-import Player from "../player.js";
+import { Player, PlayerBot } from "../player.js";
+import Gameboard from "../gameboard.js";
 
-describe("Player class defines if player is human or bot", () => {
-  const testPlayer1 = new Player("human");
-  const testPlayer2 = new Player("bot");
+describe("Player class has a gameboard", () => {
+  const testPLayer1 = new Player();
 
-  test("Player is defined as human or bot", () => {
-    expect(testPlayer1.type).toBe("human");
-    expect(testPlayer2.type).toBe("bot");
+  test("Gameboard matches Gameboard class", () => {
+    expect(testPLayer1.gameboard).toBeInstanceOf(Gameboard);
+  });
+});
+
+describe("PlayerBot class is subclass of Player, that can make automatic random plays", () => {
+  const testBot1 = new PlayerBot();
+
+  test("PLayerBot is subclass of Player", () => {
+    expect(Object.getPrototypeOf(PlayerBot)).toBe(Player);
+  });
+
+  test("Attack by PlayerBot returns valid random x & y coordinates", () => {
+    const results = testBot1.sendRandomAttack();
+
+    expect(results.randomX).toBeGreaterThanOrEqual(0);
+    expect(results.randomX).toBeLessThan(10);
+    expect(results.randomY).toBeGreaterThanOrEqual(0);
+    expect(results.randomY).toBeLessThan(10);
+  });
+
+  test("PlayerBot doesn't repeat attacks to same coordinates", () => {
+    function isRepeated() {
+      const listOfAttacks = [];
+
+      for (let i = 0; i < 99; i++) {
+        const results = testBot1.sendRandomAttack();
+        listOfAttacks.push(results);
+      }
+
+      while (listOfAttacks.length > 1) {
+        for (let j = 0; j < listOfAttacks.length - 1; j++) {
+          if (
+            listOfAttacks[listOfAttacks.length - 1].randomX ===
+              listOfAttacks[j].randomX &&
+            listOfAttacks[listOfAttacks.length - 1].randomY ===
+              listOfAttacks[j].randomY
+          ) {
+            return true;
+          }
+        }
+        listOfAttacks.pop(listOfAttacks[listOfAttacks.length - 1]);
+      }
+      return false;
+    }
+
+    const repeated = isRepeated();
+    expect(repeated).toBeFalsy();
   });
 });
